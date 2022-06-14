8bd1c21f22c4c12bf95c1814b47fa61262cd848b
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-06-14 15:00:22 +0100

    Added investment-plans.php

--- js/investment-plans-form-updater.js
+++ js/investment-plans-form-updater.js
@@ -1,3 +1,5 @@
+const numberOfInvestmentPlans = 3;
+
 let minimumMonthlyInvestmentAmountInputs = [];
 let maximumMonthlyInvestmentAmountInputs = [];
 let minimumLoanEntitledInputs = [];
@@ -5,21 +7,70 @@ let maximumLoanEntitledInputs = [];
 let loanInterestRateInputs = [];
 
 setInputs();
-
-console.log(minimumMonthlyInvestmentAmountInputs);
-console.log(maximumMonthlyInvestmentAmountInputs);
-console.log(minimumLoanEntitledInputs);
-console.log(maximumLoanEntitledInputs);
-console.log(loanInterestRateInputs);
+makeInputsReadOnly();
 
 function setInputs() {
-    const numberOfInvestmentPlans = 3;
-
     for (let i = 0; i < numberOfInvestmentPlans; i++) {
         minimumMonthlyInvestmentAmountInputs[i] = document.getElementById((i + 1) + "-minimum-monthly-investment-amount");
         maximumMonthlyInvestmentAmountInputs[i] = document.getElementById((i + 1) + "-maximum-monthly-investment-amount");
         minimumLoanEntitledInputs[i] = document.getElementById((i + 1) + "-minimum-loan-entitled");
         maximumLoanEntitledInputs[i] = document.getElementById((i + 1) + "-maximum-loan-entitled");
         loanInterestRateInputs[i] = document.getElementById((i + 1) + "-loan-interest-rate");
+    }
+}
+
+function makeInputsReadOnly() {
+    for (let i = 0; i < numberOfInvestmentPlans; i++) {
+        if (i > 0) {
+            minimumMonthlyInvestmentAmountInputs[i].setAttribute("readonly", "");
+            maximumLoanEntitledInputs[i].setAttribute("readonly", "");
+        }
+
+        maximumMonthlyInvestmentAmountInputs[i].setAttribute("readonly", "");
+        minimumLoanEntitledInputs[i].setAttribute("readonly", "");
+    }
+}
+
+function setInputLimits() {
+    let minimumMonthlyAmounts = [0, 0, 0];
+    let maximumMonthlyAmounts = [0, 0, 0];
+    let minimumLoansEntitled = [0, 0, 0];
+    let maximumLoansEntitled = [0, 0, 0];
+    let minimumLoanInterestRates = [0, 0, 0];
+    let maximumLoanInterestRates = [0, 0, 0];
+
+    minimumLoanInterestRates[2] = 10;
+    maximumLoanInterestRates[2] = 20;
+    minimumLoanInterestRates[1] = 8;
+    maximumLoanInterestRates[1] = minimumLoanInterestRates[2] - 0.01;
+    minimumLoanInterestRates[0] = 5;
+    maximumLoanInterestRates[0] = minimumLoanInterestRates[1] - 0.01;
+
+
+    for (let i = 0; i < minimumLoansEntitled.length; i++) {
+        minimumLoansEntitled[0] = 200_000;
+    }
+    
+    minimumMonthlyAmounts[0] = 10_000;
+    maximumMonthlyAmounts[0] = minimumMonthlyInvestmentAmountInputs[0].value + 15_000;
+
+    minimumMonthlyAmounts[1] = maximumMonthlyAmounts[0];
+    maximumMonthlyAmounts[1] = minimumMonthlyAmounts[1] + 25_000;
+    maximumLoansEntitled[1] = maximumLoanEntitledInputs[0].value + 300_000;
+
+    minimumMonthlyAmounts[2] = maximumMonthlyAmounts[1];
+    maximumMonthlyAmounts[2] = minimumMonthlyAmounts[1] + 25_000;
+    maximumLoansEntitled[2] = maximumLoanEntitledInputs[1].value + 300_000;
+
+    for (let i = 0; i < numberOfInvestmentPlans; i++) {
+        if (i > 0) {
+            minimumMonthlyInvestmentAmountInputs[i].value = maximumMonthlyAmounts[i - 1];
+            maximumLoanEntitledInputs[i].value = maximumLoansEntitled[i - 1];
+        }
+
+        maximumMonthlyInvestmentAmountInputs[i].value = maximumMonthlyAmounts[i];
+
+        loanInterestRateInputs[i].setAttribute("min", minimumMonthlyAmounts[i]);
+        loanInterestRateInputs[i].setAttribute("max", maximumMonthlyAmounts[i]);
     }
 }
